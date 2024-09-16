b34175d9c3419314f4be55f760e0de43d7d50cae
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-09-16 23:37:16 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from "./common/styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App;
---
+++ src/common/styles/GlobalStyles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+}
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0; 
+}
+
+button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;  
+    font: inherit;
+    color: inherit;
+    text-align: center;
+    cursor: pointer;
+    appearance: none;
+}
+`;
