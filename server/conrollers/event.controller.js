ecdfde1be9276c62cde76bc5107c3560b84dae6a
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-09-23 20:46:19 +0300

    backend/event. Доработаны эндпоинты

--- server/conrollers/event.controller.js
+++ server/conrollers/event.controller.js
@@ -2,8 +2,11 @@ const db = require('../db');
 
 class EventController {
     async createEvent(req, res) {
-        const {title, description, location} = req.body;
-        const newEvent = await db.query(`INSERT INTO event (title, location, description) values ($1, $2, $3) RETURNING *`, [title, location, description]);
+        const { title, description, location, start, finish } = req.body;
+        const newEvent = await db.query(`
+            INSERT INTO event (title, location, description, start, finish, favourite, click, want)
+            values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+            [title, location, description, start, finish, 0, 0, 0]);
         res.json(newEvent.rows)
     }
 
